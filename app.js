52c1341a9ec208e3a3d4bf0784966dfbe55b64ed
qizhen1004 <huangqz94@gmail.com>
2020-05-10 20:31:08 -0400

    initial deploy commit

---
+++ app.js
@@ -0,0 +1,59 @@
+var express       = require('express');
+var app           = express();
+var bodyParser 	  = require('body-parser');
+var mongoose      = require('mongoose');
+var flash		  = require('connect-flash');
+var passport      = require('passport');
+var LocalStrategy = require("passport-local");
+var Campground    = require("./models/campground");
+var Comment 	  = require("./models/comment");
+var User 		  = require("./models/user");
+var seedDB 		  = require("./seeds");
+var methodOverride = require("method-override");
+
+// requiring routes
+var campgroundRoutes = require("./routes/campgrounds");
+var commentRoutes = require("./routes/comments");
+var indexRoutes = require("./routes/index");
+
+// seedDB(); //seed the database
+
+// connect with databse
+mongoose.set('useFindAndModify', false)
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {'useNewUrlParser': true});
+// set up body parser
+app.use(bodyParser.urlencoded({extended: true})); 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// passport config
+app.use(require("express-session")({
+	// secrete: can be anything you want
+	secret: "Once again Evan Lin is the best!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// User.authenticate() comes from passport-local-mongoose, otherwise need to write it by self
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+	res.locals.currentUser = req.user;
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
+	next();
+});
+
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+app.use("/", indexRoutes);
+
+app.listen(3000, function() {
+	console.log("YelpCamp server is started!")
+});
---
+++ seeds.js
@@ -0,0 +1,70 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment   = require("./models/comment");
+ 
+var data = [
+    {
+        name: "Cloud's Rest", 
+        image: "https://images.unsplash.com/photo-1506535995048-638aa1b62b77?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Desert Mesa", 
+        image: "https://images.unsplash.com/photo-1519395612667-3b754d7b9086?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Canyon Floor", 
+        image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+    },
+    {
+        name: "Buffalo Bill State Park", 
+        image: "https://images.unsplash.com/photo-1519981337-7295e387c157?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        description: "The busy camping season unofficially starts Memorial Day weekend; however, we are excited to announce that we have been working with the Governor’s office and the State Health Officer to get Wyoming residents camping in just over two weeks, said Darin Westby, Director of Wyoming State Parks & Cultural Resources. The decision to reopen out-of-state visitor camping will be continually reassessed by State Park management, and a future plan will be submitted to the Governor and State Health Officer as camping begins to open in surrounding states."
+    }
+]
+ 
+function seedDB(){
+   //Remove all campgrounds
+   Campground.deleteMany({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        Comment.deleteMany({}, function(err) {
+            if(err){
+                console.log(err);
+            }
+            console.log("removed comments!");
+             //add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log("added a campground");
+                        //create a comment
+                        Comment.create(
+                            {
+                                text: "This place is great, but I wish there was internet",
+                                author: "Homer"
+                            }, function(err, comment){
+                                if(err){
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        });
+        
+    }); 
+    //add a few comments
+}
+ 
+module.exports = seedDB;
